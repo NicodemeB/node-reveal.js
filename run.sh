7651b8a9b8259196a86f3419c6f26f461b28961f
Benjamin Nicodeme <benjamin.nicodeme@icloud.com>
2019-12-21 14:41:13 +0100

    run script deleted

--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -d  -v "$(pwd)/index.html:/reveal.js/index.html" -v "$(pwd)/slides/:/reveal.js/slides/" -v "$(pwd)/bookmarks/:/reveal.js/bookmarks/" -p "127.0.0.1:1234:80" "node-reveal.js:test"
