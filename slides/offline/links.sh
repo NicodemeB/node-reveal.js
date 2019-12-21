58f5cadd6a640fbe5d863249331f493704d792f7
Benjamin Nicodeme <benjamin.nicodeme@icloud.com>
2019-12-21 15:06:12 +0100

    links.sh upated

--- slides/offline/links.sh
+++ slides/offline/links.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-ls  ../ > tmp.txt
-sed 's/offline/ /' tmp.txt > tmp2.txt
-
+ls  ../ > tmp.txt
 while IFS= read -r file; do
-    if [ "$file" != " " ] ;then
-      sed 's=https://annuel.framapad.org/p/=./offline/=g' ../$file > ../$file.tmp
-      sed 's=/export/txt=.md=g' ../$file.tmp >> ../$file.tmp2
+    if [[ "$file" =~ \.html$ ]] ;then
+      sed 's=https://annuel.framapad.org/p/=./offline/=g' ../$file > ../$file.tmp
+      sed 's=/export/txt=.md=g' ../$file.tmp >> ../$file.tmp2
+      echo $file
+      mv ../$file.tmp2 ../$file
+      rm ../$file.tmp
+    fi
+done < tmp.txt
 
-      mv ../$file.tmp2 ../$file
-      rm ../$file.tmp
-    fi
-done < tmp2.txt
-
-rm tmp.txt tmp2.txt
-
+rm tmp.txt
